dee258200df1416ce3ac593df54f4dbda80330b7
Enderman-Somnath <91421328+Enderman-Somnath@users.noreply.github.com>
2024-04-21 19:19:38 +0530

    Create Carla.bat

---
+++ Carla.bat
@@ -0,0 +1,5 @@
+\NameOfEnv\Scripts\activate
+python simple_Carla1.py --filter vehicle.bmw.grandtourer --no-autopilot
+python simple_Carla1.py --filter vehicle.kawasaki.ninja --no-autopilot
+python simple_Carla1.py
+python dynamic_weather.py --speed 10
